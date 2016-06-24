aebbfa9e770930cbfed8b70188cbe6d65131e5ba
Chris Young <beesucker@gmail.com>
2016-06-23 22:51:02 -0700

    clean out repos after test runs

--- spec/domain_helper.rb
+++ spec/domain_helper.rb
@@ -1,4 +1,8 @@
 RSpec.configure do |config|
   config.after(:each) do
+    Fixtures::Repository.delete_all
+    Pitches::Repository.delete_all
+    Players::Repository.delete_all
+    Teams::Repository.delete_all
   end
 end
