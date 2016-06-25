8915bcd2d147e2a6b475539f19c56fe44e0fb4e2
Chris Young <beesucker@gmail.com>
2016-06-25 12:43:21 -0700

    update result spec to handle 0 goals, 1 goal, many goals by the same team

--- app/domain/fixtures/result.rb
+++ app/domain/fixtures/result.rb
@@ -3,20 +3,32 @@ module Fixtures
     attr_accessor :goals, :winner, :type
     def initialize(goals:)
       @goals = goals
+      configure_as_tie && return if goals.empty?
+      configure_as_win_lose && return if goals.count == 1
+      configure_as_win_lose && return if goals.map{|goal| goal.player.team }.uniq.count == 1
+      configure_as_tie && return if team_goals.count == 1
+      configure_as_win_lose
+    end
+
+    private
+
+    def configure_as_win_lose
+      @winner = sorted_team_goals.first[1]
+      @type = "win/lose"
+    end
+
+    def configure_as_tie
       @winner = nil
       @type = "tie"
+    end
 
-      return [] if goals.empty?
-
-      team_goals = @goals.group_by{ |goal| goal.player.team }
+    def team_goals
+      @goals.group_by{ |goal| goal.player.team }
         .map{|team, goals| [goals.count, team]}.to_h
+    end
 
-      if team_goals.count == 1
-      else
-        team_goals = team_goals.to_a.sort.reverse
-        @winner = team_goals.first[1]
-        @type = "win/lose"
-      end
+    def sorted_team_goals
+      team_goals.to_a.sort.reverse
     end
   end
 end
