e41adb0fdae09648f464b68f4de18f11ef123b0e
Chris Young <beesucker@gmail.com>
2016-06-29 21:53:07 -0700

    initialize soccer_hexagon

--- lib/soccer_hexagon.rb
+++ lib/soccer_hexagon.rb
@@ -2,7 +2,7 @@ Dir[File.dirname(__FILE__) + "/soccer_hexagon/domain/**/*.rb"].each do |file|
   require_relative file
 
   class SoccerHexagon
-    def self.repositories(repositories=nil)
+    def initialize(repositories=nil)
       unless repositories.nil?
         @matches = Matches::App.new(repository: repositories::Match)
         @pitches = Pitches::App.new(repository: repositories::Pitch)
@@ -12,11 +12,11 @@ Dir[File.dirname(__FILE__) + "/soccer_hexagon/domain/**/*.rb"].each do |file|
       end
     end
 
-    def self.get_a_list_of_matches
+    def get_a_list_of_matches
       @matches.query
     end
 
-    def self.create_a_match(on:, between:, at_location:)
+    def create_a_match(on:, between:, at_location:)
       pitch = @pitches.read(at_location)
       teams = [between.first, between.last]
       @matches.create(pitch: pitch, teams: teams, datetime: on)
