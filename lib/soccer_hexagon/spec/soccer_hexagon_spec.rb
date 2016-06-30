800e75af3d624474c3c50b0ad1e35878ddaa6ce4
Chris Young <beesucker@gmail.com>
2016-06-29 21:55:23 -0700

    fix spec

--- lib/soccer_hexagon/spec/soccer_hexagon_spec.rb
+++ lib/soccer_hexagon/spec/soccer_hexagon_spec.rb
@@ -2,13 +2,12 @@ describe SoccerHexagon do
 
   let(:teams) { [double('Team'), double('Team')] }
   let(:pitch) { double('Pitch') }
+  subject { described_class.new }
+
 
   describe "#create_a_match" do
-    before do
-      described_class.repositories
-    end
     it do
-      described_class.create_a_match(
+      subject.create_a_match(
       on: Date.today,
       between: teams,
       at_location: pitch)
