b1f6904f316fcb804396c4bc4410fa19f8648f2a
Chris Young <beesucker@gmail.com>
2016-06-29 17:28:18 -0700

    use default repo if none specified

--- lib/soccer_hexagon.rb
+++ lib/soccer_hexagon.rb
@@ -1,11 +1,15 @@
 Dir[File.dirname(__FILE__) + "/soccer_hexagon/domain/**/*.rb"].each do |file|
   require_relative file
 
-  module SoccerHexagon
-    def self.repositories(repositories)
-      @fixtures = repositories::Fixture
-      @matches = Matches::App.new(repository: repositories::Match)
-      @pitches = Pitches::App.new(repository: repositories::Pitch)
+  class SoccerHexagon
+    def self.repositories(repositories=nil)
+      unless repositories.nil?
+        @matches = Matches::App.new(repository: repositories::Match)
+        @pitches = Pitches::App.new(repository: repositories::Pitch)
+      else
+        @matches = Matches::App.new
+        @pitches = Pitches::App.new
+      end
     end
 
     def self.get_a_list_of_matches
