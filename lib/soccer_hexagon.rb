6a69418533368b77cf830c293edc408d60baa125
Chris Young <beesucker@gmail.com>
2016-07-02 09:34:45 -0700

    dont include class definition in a loop

--- lib/soccer_hexagon.rb
+++ lib/soccer_hexagon.rb
@@ -1,25 +1,31 @@
 Dir[File.dirname(__FILE__) + "/soccer_hexagon/domain/**/*.rb"].each do |file|
   require_relative file
+end
 
-  class SoccerHexagon
-    def initialize(repositories=nil)
-      unless repositories.nil?
-        @matches = Matches::App.new(repository: repositories::Match)
-        @pitches = Pitches::App.new(repository: repositories::Pitch)
-      else
-        @matches = Matches::App.new
-        @pitches = Pitches::App.new
-      end
+class SoccerHexagon
+  def initialize repositories=nil
+    if repositories
+      @repositories = repositories.constants.map{|repo| [repo.to_s.downcase, 0] }.to_h
+    else
+      @repositories = self.class.constants.map do |c|
+        [c.downcase, Object.const_get('SoccerHexagon::' + c.to_s + '::App').new]
+      end.to_h
     end
+  end
 
-    def get_a_list_of_matches
-      @matches.query
-    end
+  def get_a_list_of_matches
+    repos[:matches].query
+  end
 
-    def create_a_match(on:, between:, at_location:)
-      pitch = @pitches.read(at_location)
-      teams = [between.first, between.last]
-      @matches.create(pitch: pitch, teams: teams, datetime: on)
-    end
+  def create_a_match(on:, between:, at_location:)
+    pitch = repos[:pitches].read(at_location)
+    teams = [between.first, between.last]
+    repos[:matches].create(pitch: pitch, teams: teams, datetime: on)
+  end
+
+  private
+
+  def repos
+    @repositories
   end
 end
--- lib/soccer_hexagon/spec/soccer_hexagon_spec.rb
+++ lib/soccer_hexagon/spec/soccer_hexagon_spec.rb
@@ -1,15 +1,14 @@
 describe SoccerHexagon do
-
   let(:teams) { [double('Team'), double('Team')] }
   let(:pitch) { double('Pitch') }
-  subject { described_class.new }
+  subject     { described_class.new }
 
 
   describe "#create_a_match" do
     it do
       subject.create_a_match(
-        on: Date.today,
-        between: teams,
+        on:          Date.today,
+        between:     teams,
         at_location: pitch)
     end
   end
