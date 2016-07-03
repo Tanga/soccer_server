33b55bf2169642ed4420765575c0ee00439ae104
Chris Young <beesucker@gmail.com>
2016-07-03 16:36:18 -0700

    play with real usecase

--- lib/soccer_hexagon/use_cases/create_a_match.rb
+++ lib/soccer_hexagon/use_cases/create_a_match.rb
@@ -1,20 +1,35 @@
 class SoccerHexagon
   module UseCases
+    # The objective of use case patterns is to
+    # bridge the gap between business and
+    # system models without altering requirements
+    # contents nor preempting architectural options.
+
     class CreateAMatch
       def initialize(repositories=Utilities::Repositories.new)
         @repositories = repositories
+        @errors = []
       end
 
       def call(on:, between:, at_location:)
-        @repositories[:matches].create(
-          pitch:    pitch(at_location),
-          teams:    between,
-          datetime: on)
+        tap do
+          @errors << 'No location was supplied'
+
+          pitch = get_pitch(at_location)
+          create_match(pitch, between, at_location)
+        end
       end
 
       private
 
-      def pitch(id)
+      def create_match(pitch, between, on)
+        @repositories[:matches].create(
+          pitch:    pitch,
+          teams:    between,
+          datetime: on)
+      end
+
+      def get_pitch(id)
         @repositories[:pitches].read(id)
       end
     end
