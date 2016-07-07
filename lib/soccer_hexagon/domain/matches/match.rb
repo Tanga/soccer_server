00ff98cdbcc11872855f4f862fb40351db12cae3
Chris Young <beesucker@gmail.com>
2016-07-06 18:47:37 -0700

    add some invariants to the match

--- lib/soccer_hexagon/domain/matches/match.rb
+++ lib/soccer_hexagon/domain/matches/match.rb
@@ -7,6 +7,7 @@ class SoccerHexagon
 
       class Match
         attr_accessor :id, :pitch, :teams, :datetime, :goals, :result
+
         def initialize(id:, pitch:, teams:, datetime:)
           @id = id
           @pitch = pitch
@@ -14,6 +15,12 @@ class SoccerHexagon
           @datetime = datetime
           @goals = []
           @result = Matches::Result.new(goals: @goals)
+        end
+
+        def self.invariants
+          { has_a_pitch:          'Should have a pitch',
+            is_between_two_teams: 'Should be between two teams',
+            has_a_date_and_time:  'Should have a date and time' }
         end
 
         def goals= goals
