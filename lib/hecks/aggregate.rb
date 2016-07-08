fa8820d080456a9bbfea43b16d767ee863f32848
Chris Young <beesucker@gmail.com>
2016-07-08 16:34:14 -0700

    add a create use case and test to the generators

--- lib/hecks/aggregate.rb
+++ lib/hecks/aggregate.rb
@@ -2,41 +2,15 @@
     include Thor::Actions
 
     argument :hexagon_name, type: :string
-    argument :module_name
+    argument :name
     argument :head_name
     argument :attributes, type: :hash
-
-    class_option :test_framework, :default => :rspec
 
     def self.source_root
       File.dirname(__FILE__)
     end
 
-    def domain_path
-      "lib/#{hexagon_name}/domain"
-    end
-
-    def spec_path
-      "lib/#{hexagon_name}/spec/domain"
-    end
-
-    def create_head
-      template('templates/aggregate_head.tt', "#{domain_path}/#{name}/#{head_name}.rb")
-    end
-
-    def create_head_spec_file
-      template('templates/spec/aggregate_head.tt', "#{spec_path}/#{name}/#{head_name}_spec.rb")
-    end
-
-    def create_head_behavior_file
-      template('templates/spec/head_behavior.tt', "#{spec_path}/#{name}/#{head_name}_behavior.rb")
-    end
-
-    def create_repository
-      template('templates/repository.tt', "#{domain_path}/#{name}/repository.rb")
-    end
-
-    def create_repository_spec_file
-      template('templates/spec/repository.tt', "#{spec_path}/#{name}/repository_spec.rb")
+    def create_aggregate_folder
+      directory('templates/aggregate', "lib/#{hexagon_name}")
     end
   end
